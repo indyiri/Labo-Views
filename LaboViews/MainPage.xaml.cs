581ee79ebf2dbb91b29ae17928fb65a86e52337c
Indy Iri <12402665@student.pxl.be>
2025-10-06 12:15:44 +0200

    labo afgewerkt

--- LaboViews/MainPage.xaml.cs
+++ LaboViews/MainPage.xaml.cs
@@ -1,10 +1,35 @@
-﻿namespace LaboViews
+﻿
+namespace LaboViews
 {
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
             InitializeComponent();
+            cardGrid.BackgroundColor = Colors.Black;
+        }
+
+        int tappedCount = 0;
+        private async void TapGestureRecognizer_Tapped(object sender, TappedEventArgs e)
+        {
+            Border? borderTapped = sender as Border;
+
+            if (borderTapped != null)
+            {
+                await borderTapped.RotateYTo(360, 1200);
+                borderTapped.RotationY = 0;
+            }
+
+            tappedCount++;
+
+            if (tappedCount % 2 == 0)
+            {
+                cardGrid.BackgroundColor = Colors.Black;
+            }
+            else
+            {
+                cardGrid.BackgroundColor = Colors.DarkGray;
+            }
         }
     }
 
